6ebf54a401eefe02d272a73cb044c6767354e4a8
AnshumatDinesh <anshumatdinesh@gmail.com>
2022-07-17 15:34:01 +0530

    commiting

--- C/STACK/postfix.c
+++ C/STACK/postfix.c
@@ -1,8 +1,13 @@
-#include"stack.c"
+#include"stack.c"//Importing the stack ADT
+/*
+The following function evaluates an expression written in postfix
+And returns its integer value
+It employs a stack to Evaluate the expression
+*/
 int postfix(char *expression){
-    Stack order;
-    initialize(&order);
-    int j=0;
+    Stack order;//Declaring a stack to store the numbers
+    initialize(&order);//initializing the stack
+    int j=0;//
     while(*(expression+j)!='\0'){
         int a,b,c;
         if (*(expression+j)>=0x30 AND *(expression+j)<=0x39){
@@ -31,7 +36,7 @@ int postfix(char *expression){
             case '/':
                 a=pop(&order);
                 b=pop(&order);
-                c=b/a;
+                c=b/(float)a;
                 push(&order,c);
                 break;
             
@@ -43,4 +48,10 @@ int postfix(char *expression){
         j++;
     }
     return pop(&order);
+}
+int main(){
+    char exp[100];
+    printf("Enter the Expression: \n");
+    scanf("%s",exp);
+    printf("%d",postfix(exp));
 }
